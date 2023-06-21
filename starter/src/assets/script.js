2478e29cdea200a190189d58b405659946fa2aba
Jasminedm <mooredjasmine01@gmail.com>
2023-06-21 08:56:41 -0400

    Fixing pay function and variable declarations

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,5 +1,3 @@
-/* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
-
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
    - name: name of product (string)
@@ -7,6 +5,11 @@
    - quantity: quantity in cart should start at zero (number)
    - productId: unique id for the product (number)
    - image: picture of product (url string)
+*/
+/* Images provided in /images folder. All images from Unsplash.com
+   - cherry.jpg by Mae Mu
+   - orange.jpg by Mae Mu
+   - strawberry.jpg by Allec Gomes
 */
 // REFACTORED OBJECT CREATION
 /*
@@ -23,6 +26,7 @@ const product1 = new Product("Cherry", 4, 1, 11, "./images/cherry.jpg")
 const product2 = new Product("Orange", 3, 1, 12, "./images/orange.jpg")
 const product3 = new Product("Strawberry", 5, 1, 13, "./images/strawberry.jpg")
 */
+
 //OBJECT LITERAL NOTATION
 const product1 = {
   name: "Cherry",
@@ -46,22 +50,21 @@ const product3 = {
   image: "./images/strawberry.jpg",
 };
 
-let products = [product1, product2, product3];
-/* Images provided in /images folder. All images from Unsplash.com
-   - cherry.jpg by Mae Mu
-   - orange.jpg by Mae Mu
-   - strawberry.jpg by Allec Gomes
-*/
+
+/* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
+const products = [product1, product2, product3];
+
 
 /* Declare an empty array named cart to hold the items in the cart */
-let cart = [];
+const cart = [];
+
+
+
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
-
-
 function addProductToCart(productId) {
   let product;
 
@@ -78,11 +81,13 @@ function addProductToCart(productId) {
     cart.push(product);
   }
 }
+
+
+
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
-
 function increaseQuantity(productId) {
   let product;
   if (productId === product1.productId) {
@@ -95,6 +100,8 @@ function increaseQuantity(productId) {
 
   product.quantity += 1;
 }
+
+
 
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
@@ -118,6 +125,9 @@ function decreaseQuantity(productId) {
     cart.splice(index, 1);
   }
 }
+
+
+
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
@@ -138,12 +148,13 @@ function removeProductFromCart(productId) {
   let index = cart.indexOf(product);
   cart.splice(index, 1);
 }
+
+
+
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total of all products
   - cartTotal should return the sum of the products in the cart
 */
-
-
 function cartTotal() {
   let total = 0;
 
@@ -152,25 +163,33 @@ function cartTotal() {
   });
   return total;
 }
+
+
+
 /* Create a function called emptyCart that empties the products from the cart */
 function emptyCart() {
   cart = [];
 }
+
+
+
 /* Create a function named pay that takes in an amount as an argument
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
 */
-
 function pay(totalPaid) {
   let total = cartTotal();
 
-  if (total > totalPaid) {
-    return totalPaid - total;
-  } else {
-    return total - totalPaid;
-  }
+  return totalPaid - total
 }
+
+
+
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
+
+
+
+
 
 /* The following is for running unit tests. 
    To fully complete this project, it is expected that all tests pass.
